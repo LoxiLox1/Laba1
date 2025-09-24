4e478aaed9df05a04f6911497813b5bd81b1cfe1
LoxiLox1 <vitol6215@gmail.com>
2025-09-24 11:41:55 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("апыпыпк (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Вапоаооапоапов (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[m, n];
+
+        Console.WriteLine("Ввераеаераопоаноаоанрматрицы:");
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"Элемент [{i + 1},{j + 1}]: ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nВаша ыппвырвыр:");
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        // Пример вычисления: сумма всех элементов
+        int sum = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                sum += matrix[i, j];
+
+        Console.WriteLine($"\nбебесбабаб {sum}");
+    }
+}
