25e5289483dfbc869b2081b018d2d031b5a73605
LoxiLox1 <vitol6215@gmail.com>
2025-09-24 11:38:47 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("апыпыпк (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите ыпыпыыврыпрыролбцов (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[m, n];
+
+        Console.WriteLine("Введите элементы матрицы:");
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"Элемент [{i + 1},{j + 1}]: ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nВаша матрица:");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        // Пример вычисления: сумма всех элементов
+        int sum = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                sum += matrix[i, j];
+
+        Console.WriteLine($"\nбебесбабаб {sum}");
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Простой калькулятор на C#");
+        Console.WriteLine("Доступные операции: +, -, *, /");
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("\nВведите первое число: ");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите операцию (+, -, *, /): ");
+                string op = Console.ReadLine();
+
+                Console.Write("Введите второе число: ");
+                double num2 = double.Parse(Console.ReadLine());
+
+                double result = 0;
+
+                switch (op)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 != 0)
+                            result = num1 / num2;
+                        else
+                        {
+                            Console.WriteLine("Ошибка: деление на ноль!");
+                            continue;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка: неизвестная операция!");
+                        continue;
+                }
+
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода! Попробуйте снова.");
+            }
+
+            Console.Write("\nХотите продолжить? (y/n): ");
+            string choice = Console.ReadLine().ToLower();
+            if (choice != "y")
+                break;
+        }
+    }
+}
